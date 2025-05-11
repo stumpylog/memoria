7aaef1d1753e7a63c450cd0db62c4dda70defb69
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2025-05-11 10:00:45 -0700

    Formats css and js with prettier

--- memoria/static/js/theme-picker.js
+++ memoria/static/js/theme-picker.js
@@ -1,18 +1,21 @@
-(function() {
-    const storedTheme = localStorage.getItem('theme');
+(function () {
+  const storedTheme = localStorage.getItem("theme");
 
-    const getPreferredTheme = () => {
-        if (storedTheme) {
-            return storedTheme;
-        }
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  const getPreferredTheme = () => {
+    if (storedTheme) {
+      return storedTheme;
     }
+    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  };
 
-    const theme = getPreferredTheme();
-    if (theme === 'system') { // 'system' stored value needs to translate to 'light' or 'dark' for the attribute
-            document.documentElement.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-    } else {
-        document.documentElement.setAttribute('data-bs-theme', theme);
-    }
-
+  const theme = getPreferredTheme();
+  if (theme === "system") {
+    // 'system' stored value needs to translate to 'light' or 'dark' for the attribute
+    document.documentElement.setAttribute(
+      "data-bs-theme",
+      window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light",
+    );
+  } else {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+  }
 })();
