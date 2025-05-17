f9feba22140803a5f99130adf97c8d26a5bc5364
Trenton H <797416+stumpylog@users.noreply.github.com>
2025-05-16 19:14:26 -0700

    Try to fix Huey

---
+++ memoria/tasks/__init__.py
@@ -0,0 +1,5 @@
+from memoria.tasks.images import index_image_batch
+from memoria.tasks.images import index_replace_existing_images
+from memoria.tasks.images import index_update_existing_images
+
+__all__ = ["index_image_batch", "index_replace_existing_images", "index_update_existing_images"]
