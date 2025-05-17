5e95a32c4f1ac18300a73d48bf249c1300371e95
Trenton H <797416+stumpylog@users.noreply.github.com>
2025-05-17 09:39:20 -0700

    More Bash to Ash corrections.  Also import tasks into the AppConfig

--- memoria/apps.py
+++ memoria/apps.py
@@ -10,3 +10,6 @@ class MemoriaAppConfig(AppConfig):
         from memoria.signals.handlers import mark_image_as_dirty  # noqa: F401
         from memoria.signals.handlers import mark_images_as_dirty_on_fk_change  # noqa: F401
         from memoria.signals.handlers import mark_images_as_dirty_on_m2m_change  # noqa: F401
+        from memoria.tasks import index_image_batch  # noqa: F401
+        from memoria.tasks import index_replace_existing_images  # noqa: F401
+        from memoria.tasks import index_update_existing_images  # noqa: F401
